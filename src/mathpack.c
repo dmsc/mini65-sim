a68a832e8e217f554e038d9b329e577e94c52dd1
Daniel Serpell <daniel.serpell@gmail.com>
2018-10-15 01:27:57 -0300

    Add license headers.

--- src/mathpack.c
+++ src/mathpack.c
@@ -1,8 +1,26 @@
+/*
+ * Mini65 - Small 6502 simulator with Atari 8bit bios.
+ * Copyright (C) 2017,2018 Daniel Serpell
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 #include "mathpack.h"
 #include "mathpack_bin.h"
 
 int fp_init(sim65 s)
 {
+    // Simply loads Altirra mathpack into correct address.
     sim65_add_data_rom(s, 0xD800, mathpack_bin, mathpack_bin_len);
     return 0;
 }
